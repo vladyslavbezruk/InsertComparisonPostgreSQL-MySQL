55c3f2d6c4461346eac49f5cf57374c0b7c17772
Vladyslav Bezruk <bezrukvladislav@gmail.com>
2023-04-04 20:42:02 +0300

    Added tests.

---
+++ MySQL_test_threads_1.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sysbench sysbench/src/lua/oltp_insert.lua --threads=1 --events=50000 --time=0 --mysql-host=127.0.0.1 --mysql-user=vbezruk --mysql-password=12345678 --mysql-port=3306 --tables=1 --table-size=100000 --range_selects=off --db-ps-mode=disable --report-interval=1 run
---
+++ PostgreSQL_test_threads_4.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sysbench sysbench/src/lua/oltp_insert.lua --threads=4 --events=50000 --time=0 --pgsql-host=127.0.0.1 --pgsql-user=postgres --pgsql-password=12345678 --pgsql-port=5432 --tables=1 --table-size=100000 --range_selects=off --db-ps-mode=disable --report-interval=1 run
